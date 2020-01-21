764e4785a4f4a69cc9aef2312a996256757b5ca0
Dávid Csejtei <csejteidavid90@gmail.com>
2020-01-21 18:55:46 +0100

    Food

---
+++ src/Food.spec.ts
@@ -0,0 +1,23 @@
+import Food from './Food';
+
+describe('Food', () => {
+    test('create', () => {
+        const baseValues = {
+            amount: 100,
+            fat: 30,
+            carbohydrate: 40,
+            protein: 65,
+            calories: 124
+        };
+        const food = new Food('rice', 'g', baseValues);
+
+        expect(food).toBeDefined();
+        expect(food.getName()).toEqual('rice');
+        expect(food.getUnit()).toEqual('g');
+        expect(food.getBaseValues().amount).toEqual(100);
+        expect(food.getBaseValues().fat).toEqual(30);
+        expect(food.getBaseValues().carbohydrate).toEqual(40);
+        expect(food.getBaseValues().protein).toEqual(65);
+        expect(food.getBaseValues().calories).toEqual(124);
+    });
+});

8ab13f37b75461ddd9a0ba76adca377a2f468c03
Dávid Csejtei <csejteidavid90@gmail.com>
2020-02-02 17:02:45 +0100

    1.9 - implement calculate current calories

--- src/Food.ts
+++ src/Food.ts
@@ -45,6 +45,10 @@ class Food {
     changeAmount(amount: number) {
         this.validateFoodAmount(amount);
         this.currentValues.amount = amount;
+        this.currentValues.calories
+            = Math.ceil(
+                this.currentValues.amount * this.baseValues.calories
+                / this.baseValues.amount);
     }
 }
 

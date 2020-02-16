fde2e1ac1ed9dabb0113c4fe7136e0c2831d5cf8
Dávid Csejtei <csejteidavid90@gmail.com>
2020-02-16 19:44:10 +0100

    First build

---
+++ dist/Units.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Units;
+(function (Units) {
+    Units["GRAM"] = "g";
+})(Units || (Units = {}));
+;
+exports.default = Units;
